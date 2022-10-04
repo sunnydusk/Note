a7a96d7128a43417df9eecd12fb34a2dc0ea81a9
sunny <sunnydusk@outlook.com>
2022-10-04 19:59:28 +0800

    05

---
+++ js/music.js
@@ -0,0 +1,15 @@
+var music = document.getElementById('music')
+var avatar_size = document.getElementById('avatar_size')
+var tem = true
+
+function musiccc(){
+    if (tem == false){
+        music.play()
+        tem = true
+        document.getElementById('avatar_size').style.animationPlayState = 'running'
+    }else{
+        music.pause()
+        tem = false
+        document.getElementById('avatar_size').style.animationPlayState = 'paused'
+    }
+}
