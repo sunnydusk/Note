66331c2db55039c00f84e3263e3bf4943cb6780f
sunny <sunnydusk@outlook.com>
2022-10-13 19:05:16 +0800

    10

--- js/dh.js
+++ js/dh.js
@@ -1,17 +1,34 @@
-$("#music").get(0).play();
-var flag = false;
-$(".avatar_size").click(function () {
-    if (!flag) {
-        // $(this).css({"-webkit-animation-iteration-count":"0"});
-        $(this).css({ "-webkit-animation-play-state": "paused" });
-        // 音乐停止
-        $("#music").get(0).pause();
-        flag = true;
-    } else {
-        // $(this).css({"-webkit-animation-iteration-count":"infinite"});
-        $(this).css({ "-webkit-animation-play-state": "running" });
-        // 音乐播放
-        $("#music").get(0).play();
-        flag = false;
+// $("#music").get(0).play();
+// var flag = false;
+// $(".avatar_size").click(function () {
+//     if (!flag) {
+//         // $(this).css({"-webkit-animation-iteration-count":"0"});
+//         $(this).css({ "-webkit-animation-play-state": "paused" });
+//         // 音乐停止
+//         $("#music").get(0).pause();
+//         flag = true;
+//     } else {
+//         // $(this).css({"-webkit-animation-iteration-count":"infinite"});
+//         $(this).css({ "-webkit-animation-play-state": "running" });
+//         // 音乐播放
+//         $("#music").get(0).play();
+//         flag = false;
+//     }
+// })
+// $("#avatar_size").get(0).play();
+
+const avatar = document.getElementsByClassName("avatar_size")[0]
+let flag = true
+avatar.addEventListener("click", function () {
+    const music = document.getElementById("music")
+    if (flag) {
+        music.play();
+        avatar.setAttribute("style","animation-name:music")
+        flag = false
+    }
+    else {
+        music.pause();
+        avatar.setAttribute("style","animation-name:mu")
+        flag = true
     }
 })
--- js/music.js
+++ js/music.js
@@ -1,17 +1,17 @@
-var music = document.getElementById('music')
-var avatar_size = document.getElementById('avatar_size')
-var tem = true
+// var music = document.getElementById('music')
+// var avatar_size = document.getElementById('avatar_size')
+// var tem = true
 
-function musiccc() {
-    if (tem == false) {
-        music.play()
-        tem = true
-        document.getElementById('avatar_size').style.animationPlayState = 'running'
-    } else {
-        music.pause()
-        tem = false
-        document.getElementById('avatar_size').style.animationPlayState = 'paused'
-    }
-}
+// function musiccc() {
+//     if (tem == false) {
+//         music.play()
+//         tem = true
+//         document.getElementById('avatar_size').style.animationPlayState = 'running'
+//     } else {
+//         music.pause()
+//         tem = false
+//         document.getElementById('avatar_size').style.animationPlayState = 'paused'
+//     }
+// }
 
 
